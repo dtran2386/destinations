01b2d33f8a7460849db59b15d51d1890fb543470
Dylan K Tran <dtran2386@gmail.com>
2016-05-11 14:07:43 -0400

    getty images API working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=Charlotte,%20NC&appid=1f2671239fb2a0b6556a93f5873da5b1';
+var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q=Miami,%20FL&appid=1f2671239fb2a0b6556a93f5873da5b1';
+//var weatherURL = 'https://api.meetup.com/2/events?key=4a7a120354a45344b4e623c33521d69&group_urlname=ny-tech&sign=true';
 
-var app = angular.module('Destinations', ['ngRoute']);
+
+var app = angular.module('Destinations', ['ngRoute', 'ngAnimate']);
 
 app.config(['$routeProvider', function ($routeProvider) {
    $routeProvider
@@ -49,15 +51,16 @@ app.controller('HomeController', ['$scope', 'SharedStuff', function ($scope, Sha
 }]);
 
 app.controller('StatesController', ['$scope', 'SharedStuff', function ($scope, SharedStuff) {
-    console.log('done again');
+//    console.log('done again');
     $scope.images = SharedStuff.getImages();
-    console.log($scope.images);
+
 }]);
 
 app.controller('CitiesController', ['$scope', 'SharedStuff', function ($scope, SharedStuff) {
-    console.log('done yet again');
+//    console.log('done yet again');
     $scope.weather = SharedStuff.getWeather();
-    
+    $scope.images = SharedStuff.getImages();
+
 }]);
 
 app.factory('SharedStuff', function($http) {
@@ -68,8 +71,14 @@ app.factory('SharedStuff', function($http) {
         method: 'GET',
         url: weatherURL,
     }).then(function (response) {
+//        response.data.main.ftemp = Math.round(response.data.main.temp * (9/5) - 459.67);
+//        console.log(response.data);
+//        if (response.data.weather[0].description === "clear sky") {
+//            response.data.icon = "images/noun_446966_cc.png";
+//        } else if (response.data.weather[0].description === 'scattered clouds') {
+//            response.data.icon = 'images/noun_21526_cc.png';
+//        }
         weather.push(response.data);
-//        console.log(response);
     });
     
     $http({
@@ -79,7 +88,9 @@ app.factory('SharedStuff', function($http) {
             'Api-Key': 'y4qp9xe6axaccvr33qhca9fh',
         }
     }).then(function (response) {
-            images.push(response.data.images);
+        for(var i = 0; i < response.data.images.length; i++) {
+            images.push(response.data.images[i]);
+        };
 //        console.log(response);
     });
     
