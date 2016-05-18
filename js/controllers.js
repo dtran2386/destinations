0d64a369da8d33d097903823d533ab538941f8ea
Dylan K Tran <dtran2386@gmail.com>
2016-05-18 12:18:42 -0400

    hotel names successfully link to a google search result

--- js/controllers.js
+++ js/controllers.js
@@ -115,6 +115,10 @@ module.exports = (function() {
          // Instantiate a new chart using 'data' (defined below)
          var myChart = new Chart(ctx).Line(data);
          });
+        
+        
+        // Code to get Foursquare data
+        $scope.events = DestService.getEvents();
 
     }]); //<= END OF CITY VIEW CONTROLLER
 
